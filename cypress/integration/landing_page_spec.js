3c15f39232c0156d967195839bde8ec434a94f61
Sebastian Geiger <sebastian.geiger@gmail.com>
2018-05-13 13:15:48 -0700

    Test that asserts against welcome message

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -1,5 +1,7 @@
 describe('Landing Page', function () {
   it('exists', function () {
-    cy.visit('http://localhost:3030')
+    cy.visit('http://localhost:3000')
+      .get('body')
+      .should('contain', "Welcome to React")
   })
 })
